7f343e953d157c7edd0c4e3be77a8c4e71ada9d3
Ryan Paige <oshmagoshzz@gmail.com>
2021-06-18 22:22:24 -0400

    added code for decreasing # of generated paths

--- pages/epic/[id].tsx
+++ pages/epic/[id].tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 const imageLink =  'https://epic.gsfc.nasa.gov/archive/enhanced/2021/06/03/png/'
+const url = 'https://epic.gsfc.nasa.gov/api/enhanced/date/2021-06-03/?api_key=' + apiKey
 
 export default function EpicId({ epic }:any) {
     const router = useRouter();
@@ -27,15 +28,26 @@ export default function EpicId({ epic }:any) {
     )
 }
 
-export const getServerSideProps = async (context:any) => {
-    const res = await fetch(
-      `https://epic.gsfc.nasa.gov/api/enhanced/date/2021-06-03/?api_key=` + apiKey + `${context.params.id}`
-    );
+export const getStaticProps = async (context:any) => {
+    const res = await fetch(url)
     const epic = await res.json();
 
     return {
       props: {
         epic: epic[`${context.params.id}`],
       },
+    };
+};
+
+export const getStaticPaths = async () => {
+    const res = await fetch(url);
+    const epics = await res.json();
+  
+    const paths = epics.map((item:any, index:number) => ({
+      params: { id: index.toString() },
+    }));
+    return {
+      paths,
+      fallback: false,
     };
   };
