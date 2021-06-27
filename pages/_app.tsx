d0e8d5f54f1e9766870d4b9aa945e5ba7e6a01dc
Ryan Paige <oshmagoshzz@gmail.com>
2021-06-27 03:50:14 -0400

    page transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,22 @@
+import { motion } from 'framer-motion';
 import Layout from '../components/Layout'
 import '../styles/globals.sass'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
+      pageInitial: {
+        opacity: 0
+        },
+        pageAnimate: {
+          opacity: 1
+        },
+    }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </motion.div>
   )
 }
 export default MyApp
