e85e8875e3a4e3ba0da9601016a5bacd2e9725cd
Ryan Paige <oshmagoshzz@gmail.com>
2021-07-17 08:08:12 -0400

    config changes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const path = require("path");
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
 const nextConfig = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/apods": { page: "/apods" },
+      "/rover": { page: "/rover" },
+      "/epic": { page: "/epic" },
+      "/earth": { page: "/earth" },
+    };
+  },
   /* config options here */
   reactStrictMode: true,
   env: {
