6adb515ecfd0274b3d7b5ccb5ac71035fc64c060
Ryan Paige <universallyry@gmail.com>
2022-09-09 13:14:11 -0400

    refactored card, data own file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,77 +1,45 @@
 import Head from 'next/head';
-import { 
-  Image, 
-  chakra, 
-  VStack, 
-  Box, 
-  HeadingProps, 
-  Text, 
-  SimpleGrid, 
-  Heading 
+import {
+  Image,
+  chakra,
+  VStack,
+  Box,
+  SimpleGrid,
 } from "@chakra-ui/react";
-import { motion } from 'framer-motion';
 import HeroCard from '../components/HeroCard';
+import { cardInfo, CardsInfo } from '../utils/cardInfo';
 
-const MotionHeading = motion<HeadingProps>(Heading)
+
 //todos: more global styling, more loading states, more animations
 const Home = () => {
   const img = "../public/tim-tdd-QoewgBpXJd4-unsplash.jpg"
+
+  const HeroCards = () => {
+    let info = cardInfo.map((item: CardsInfo) => {
+      return (
+        <HeroCard
+          href={item.href}
+          title={item.title}
+          paragraph={item.paragraph}
+          imgSrc={item.imgSrc}
+        />
+      )
+    });
+    return info;
+  }
+
   return (
     <Box background-image={img} align="center" justify="center" direction={{ base: "column", md: "row" }}>
       <Head key='pages/index key'>
-        <title>NASA Info</title>  
+        <title>NASA Info</title>
         <meta name="description" content="Look up information NASA provides" />
       </Head>
-        <VStack>
-          <MotionHeading
-            initial="hidden" 
-            animate="visible" 
-            variants={{
-              hidden: {
-                scale: .8,
-                opacity: 0
-              },
-              visible: {
-                scale: 1,
-                opacity: 1,
-                transition: {
-                  delay: .4
-                }
-              },
-            }}
-          >
-            <Text fontSize="3xl" userSelect="none" my={5}>
-              View Satellites, Rovers and other Astronomic Pictures
-            </Text>
-          </MotionHeading>
-        <SimpleGrid 
+      <VStack mt={4}>
+        <SimpleGrid
           gap={3}
           columns={[1, 1, 2,]}
         >
-          <HeroCard
-            href='/apod'
-            title='APoD &rarr;' 
-            paragraph='Astronomy Picture of the Day.'
-            imgSrc='/apod.png'
-          />
-          <HeroCard 
-            href='/landsat'
-            title='Landsat &rarr;' 
-            paragraph='Landsat imagery is provided to the public as a joint project between NASA and USGS.'
-            imgSrc='/satellite.png'
-            />
-          <HeroCard 
-            href='/epic'
-            title='EPIC &rarr;' 
-            paragraph='Earth Polychromatic Imaging Camera aka EPIC provides full disc imagery of the Earth and captures unique perspectives of certain astronomical events such as lunar transits.'
-            imgSrc='/planet.png'
-            />
-          <HeroCard 
-            href='/rover'
-            title='Mars Rover Photos &rarr;' 
-            paragraph='Image data gathered by NASA&apos;s Curiosity, Opportunity, and Spirit rovers on Mars. Current functionality only for Curiosity camera.'
-            imgSrc='/mars-rover.png'
-          />
+          {HeroCards()}
         </SimpleGrid>
 
         <VStack my={5}>
@@ -79,10 +47,10 @@ const Home = () => {
             href="https://verscel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-            >
+          >
             Powered by{' '}
             <chakra.span>
-              <Image src="/vercel.svg" alt="Vercel Logo" htmlWidth={ 72 } htmlHeight={ 16 } />
+              <Image src="/vercel.svg" alt="Vercel Logo" htmlWidth={72} htmlHeight={16} />
             </chakra.span>
           </chakra.a>
           <chakra.div>Hero icons made by <chakra.a href="https://www.freepik.com" title="Freepik">Freepik</chakra.a> from <chakra.a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</chakra.a></chakra.div>
