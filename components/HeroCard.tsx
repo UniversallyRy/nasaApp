e4dcaa36f163527b220a40af72108b9ae1a6271d
Ryan Paige <universallyry@gmail.com>
2022-09-09 11:40:39 -0400

    spacing on card

--- components/HeroCard.tsx
+++ components/HeroCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import NextLink from "next/link";
 import Image from 'next/image';
-import { chakra, GridItem, GridItemProps, Stack, useColorModeValue, Link } from "@chakra-ui/react";
+import { chakra, GridItem, GridItemProps, Stack, useColorModeValue, Center, Link } from "@chakra-ui/react";
 import { motion } from 'framer-motion';
 
 type CardProps = {
@@ -18,46 +18,49 @@ const HeroCard = ({ ...props }: CardProps) => {
   const backGround = useColorModeValue("blue.500", "purple.900");
 
   return (
-    <MotionBox
-      whileHover={{
-        position: 'relative',
-        zIndex: "auto",
-        scale: [1, 1.02, 1],
-        transition: {
-          duration: .3
-        }
-      }}
-      mx={1}
-      rounded="sm"
-      shadow="xl"
-      bg={backGround}
-      boxSize={{ base: "sm", lg: "md" }}
-    >
-      <Stack spacing={2} align="center" p={3}>
-        <Image
-          width='200'
-          height='200'
-          src={props.imgSrc}
-          alt="Icon made by Freepik from www.flaticon.com"
-        />
-        <NextLink passHref href={props.href}>
-          <Link
-            fontSize={["xl", "2xl"]}
-            color={useColorModeValue("gray.700", "white")}
-            fontWeight="bold"
-            _hover={{
-              color: useColorModeValue("gray.600", "gray.200"),
-              textDecor: "underline",
-            }}
-          >
-            {props.title}
-          </Link>
-        </NextLink>
-        <chakra.p fontSize={{ base: 'md', md: "lg" }} m={5} color={useColorModeValue("gray.600", "gray.300")}>
-          {props.paragraph}
-        </chakra.p>
-      </Stack>
-    </MotionBox>
+    <Center>
+      <MotionBox
+        whileHover={{
+          position: 'relative',
+          zIndex: "auto",
+          scale: [1, 1.02, 1],
+          transition: {
+            duration: .3
+          }
+        }}
+        rounded="sm"
+        shadow="sm"
+        position='relative'
+        mt="3"
+        bg={backGround}
+        boxSize={{ base: "sm", lg: "md" }}
+      >
+        <Stack spacing={2} alignItems="center" p={3} mt="7">
+          <Image
+            width='200'
+            height='200'
+            src={props.imgSrc}
+            alt="Icon made by Freepik from www.flaticon.com"
+          />
+          <NextLink passHref href={props.href}>
+            <Link
+              fontSize={["xl", "2xl"]}
+              color={useColorModeValue("gray.700", "white")}
+              fontWeight="bold"
+              _hover={{
+                color: useColorModeValue("gray.600", "gray.200"),
+                textDecor: "underline",
+              }}
+            >
+              {props.title}
+            </Link>
+          </NextLink>
+          <chakra.p fontSize={{ base: 'md', md: "lg" }} m={5} color={useColorModeValue("gray.600", "gray.300")}>
+            {props.paragraph}
+          </chakra.p>
+        </Stack>
+      </MotionBox>
+    </Center>
   )
 };
 
