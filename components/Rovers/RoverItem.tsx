49c13613465bea1785a9f52bd17edf3363fe2544
Ryan Paige <universallyry@gmail.com>
2022-09-15 21:47:45 -0400

    path fix

--- components/Rovers/RoverItem.tsx
+++ components/Rovers/RoverItem.tsx
@@ -1,51 +1,51 @@
 import { Box, Text } from "@chakra-ui/react";
-import { ChakraNextImage } from "./Image";
+import { ChakraNextImage } from "../Image";
 
 type ItemProps = {
-    item: {
-        earth_date: string
-        sol: string
-        camera: {
-            name: string
-        }
-        img_src: string
-        id: string
+  item: {
+    earth_date: string
+    sol: string
+    camera: {
+      name: string
     }
-    index: number
-    slidesCount: number
+    img_src: string
+    id: string
+  }
+  index: number
+  slidesCount: number
 };
 
 const RoverItem = ({ ...props }: ItemProps) => {
-    return (
-        <Box 
-            boxSize="full"
-            fontSize="xl"
-            flex="none"
-            rounded="sm"
-            shadow="lg"
-        >
-            <Text ml={2} mt={7}>Date Taken: { props.item.earth_date }</Text>
-            <Text m={2}>Sol: { props.item.sol }</Text>
-            <Text m={2}>Camera: { props.item.camera.name }</Text>
+  return (
+    <Box
+      boxSize="full"
+      fontSize="xl"
+      flex="none"
+      rounded="sm"
+      shadow="lg"
+    >
+      <Text ml={2} mt={7}>Date Taken: {props.item.earth_date}</Text>
+      <Text m={2}>Sol: {props.item.sol}</Text>
+      <Text m={2}>Camera: {props.item.camera.name}</Text>
 
-            <ChakraNextImage
-                w='full' 
-                h={{base:"60vh", sm:"70vh", md: "70vh"}}
-                rounded="sm"
-                shadow="sm"
-                src={ props.item.img_src }
-                alt={ props.item.id }
-            />
-            <Text
-                fontSize="xs"
-                p="2"
-                pos="absolute"
-                top="0"
-            >
-                {props.index + 1} / {props.slidesCount}
-            </Text>
-        </Box>  
-    )
+      <ChakraNextImage
+        w='full'
+        h={{ base: "60vh", sm: "70vh", md: "70vh" }}
+        rounded="sm"
+        shadow="sm"
+        src={props.item.img_src}
+        alt={props.item.id}
+      />
+      <Text
+        fontSize="xs"
+        p="2"
+        pos="absolute"
+        top="0"
+      >
+        {props.index + 1} / {props.slidesCount}
+      </Text>
+    </Box>
+  )
 };
 
 export default RoverItem;
