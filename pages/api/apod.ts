aff91b57c55f94f09fe02cd02ec8fbff22313711
Ryan Paige <universallyry@gmail.com>
2022-10-05 05:13:02 -0400

    api fix

--- pages/api/apod.ts
+++ pages/api/apod.ts
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from "next"
 import { fetchedData } from '../../utils/getData';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const response = await fetchedData('apod');
     res.statusCode = 200;
