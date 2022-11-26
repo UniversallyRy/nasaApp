d7fbe1695d8894c728be0ff673944a95ad8fdfd8
Ryan Paige <universallyry@gmail.com>
2022-11-25 23:10:52 -0500

    toggle button added

--- components/NavBar/HNavButtons.tsx
+++ components/NavBar/HNavButtons.tsx
@@ -4,17 +4,18 @@ import { ButtonGroup } from "@chakra-ui/react";
 import { BsFillImageFill } from "react-icons/bs";
 import { FaSatelliteDish } from "react-icons/fa";
 import { GiEarthAmerica } from "react-icons/gi";
-import NavButton from "./NavButton";
+import NavButton, { ToggleButton } from "./NavButton";
 //      display={{ base: "none", lg: "inline-flex" }}
 
 export default function HButtons() {
   return (
-    <div className="pl-10">
+    <div className="flex flex-row px-3 pl-10">
       <NavButton href="/" title="Home" />
-      <NavButton href="/apod" title="APoD" />
+      <NavButton href="/apod" title="APOD" />
       <NavButton href="/landsat" title="Landsat" />
       <NavButton href="/epic" title="EPIC" />
       <NavButton href="/rover" title="Rover Photos" />
+      <ToggleButton />
     </div>
   );
 };
