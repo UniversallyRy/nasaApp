d94a7ae279f50b5cb304ca3109fbb48608bca367
Ryan Paige <universallyry@gmail.com>
2022-12-11 14:27:57 -0500

    active button color added

--- components/NavBar/NavButton.tsx
+++ components/NavBar/NavButton.tsx
@@ -1,18 +1,8 @@
 import NextLink from "next/link";
+import { useRouter, usePathname } from "next/navigation";
 import { IconType } from "react-icons";
 import { FaMoon, FaSun } from "react-icons/fa";
-import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 
-//      leftIcon={props.buttonIcon}
-//     ToggleButton:
-//      mx={2}
-//      p={1}
-//      _focus={{ outline: "none" }}
-///      icon={colorMode === "light" ? <FaSun /> : <FaMoon />}
-//      isRound={true}
-//      aria-label="Color Toggle"
-//      size="sm"
-//      onClick={toggleColorMode}
 type Props = {
   title: string;
   buttonIcon?: React.ReactElement<IconType>;
@@ -21,6 +11,7 @@ type Props = {
 
 export default function NavButton({ ...props }: Props) {
   const router = useRouter();
+  const pathname = usePathname();
   const label = `${props.title} navigation button`;
 
   if (label) {
@@ -28,7 +19,9 @@ export default function NavButton({ ...props }: Props) {
       <button
         onClick={() => router.push(props.href)}
         aria-label={label}
-        className="mx-2 rounded-md font-semibold bg-gray-600 w-32 :focus:outline-none hover:shadow-lg hover:shadow-gray-800"
+        className={`mx-2 rounded-md font-semibold w-32 focus:outline-none
+                   hover:shadow-md hover:shadow-gray-800 hover:bg-gray-700
+                   ${pathname == props.href ? "bg-gray-700" : "bg-gray-600"}`}
       >
         <NextLink passHref href={props.href} legacyBehavior>
           {props.title}
@@ -39,14 +32,16 @@ export default function NavButton({ ...props }: Props) {
   return null;
 }
 
-export const ToggleButton = () => {
-  // const { colorMode, toggleColorMode } = useColorMode();
+export function ToggleButton() {
   return (
     <label className="flex bg-black/10 shadow-sm relative ml-auto mr-1 justify-between rounded-lg items-center group p-2">
-      <input type="checkbox" className="left-1/2 -translate-x-1/2 peer appearance-none rounded-lg" />
+      <input
+        type="checkbox"
+        className="left-1/2 -translate-x-1/2 peer appearance-none rounded-lg"
+      />
       <FaSun className="text-gray-300 mr-2 h-6 w-6" />
       <span className="w-16 h-10 flex relative items-center flex-shrink-0 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:after:bg-black/75 peer-checked:bg-gray-700 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
       <FaMoon className="text-black-800 ml-2 h-6 w-6" />
     </label>
   );
-};
+}
