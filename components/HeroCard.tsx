30693847b03e1292612251c3fc8d9b77116a0e5b
Ryan Paige <universallyry@gmail.com>
2022-12-11 14:27:21 -0500

    centered, styling changes

--- components/HeroCard.tsx
+++ components/HeroCard.tsx
@@ -9,45 +9,20 @@ type Props = {
   imgSrc: string;
   key: string;
 };
-//   userSelect="none"
-//    whileHover={{
-//      position: "relative",
-///      zIndex: "auto",
-//     scale: [1, 1.02, 1],
-//     transition: {
-//       duration: 0.3,
-//     },
-//   }}
-//   _focus={{ outline: "none" }}
-//   rounded="md"
-//   shadow="lg"/
-//   position="relative"
-//  mt="4"
-//    bg={backGround}
-//   border="2px"
-//   borderColor="blackAlpha.100"
-//   boxSize={{ base: "sm", lg: "md" }}
+
 export default function HeroCard({ ...props }: Props) {
-
   return (
-    <div
-      className="flex flex-col mt-5 mb-1 p-5 shadow-black shadow-lg relative border-black"
-    >
-      <Image
-        width="150"
-        height="150"
-        src={props.imgSrc}
-        alt="Icon made by Freepik from www.flaticon.com"
-      />
-      <NextLink passHref href={props.href} legacyBehavior>
-        <a className="font-bold hover:underline">
-          {props.title}
-        </a>
-      </NextLink>
-      <p className="font-semibold m-5">
-        {props.paragraph}
-      </p>
-    </div>
+    <NextLink passHref href={props.href} legacyBehavior>
+      <div className="relative flex flex-col space-x-2 p-2 items-center shadow-black/20 shadow-md border-black/90 hover:bg-purple-900 hover:cursor-pointer">
+        <Image
+          width="150"
+          height="150"
+          src={props.imgSrc}
+          alt="Icon made by Freepik from www.flaticon.com"
+        />
+        <a className="m-2 font-bold">{props.title}</a>
+        <p className="m-5 font-semibold">{props.paragraph}</p>
+      </div>
+    </NextLink>
   );
-};
-
+}
