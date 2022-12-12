20c1ade0995235d69fa5c6a0dbab989b54b4b1ca
Ryan Paige <universallyry@gmail.com>
2022-12-11 20:12:06 -0500

    reformat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
-'use client'
+"use client";
 
 import HeaderNav from "../components/NavBar";
 import "../styles/globals.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
