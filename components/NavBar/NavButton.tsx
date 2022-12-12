3168ab924bea687657deb10ed8b2f048104733ea
Ryan Paige <universallyry@gmail.com>
2022-12-11 20:14:46 -0500

    checkbox hidden for tailwind/form usage

--- components/NavBar/NavButton.tsx
+++ components/NavBar/NavButton.tsx
@@ -35,10 +35,7 @@ export default function NavButton({ ...props }: Props) {
 export function ToggleButton() {
   return (
     <label className="flex bg-black/10 shadow-sm relative ml-auto mr-1 justify-between rounded-lg items-center group p-2">
-      <input
-        type="checkbox"
-        className="left-1/2 -translate-x-1/2 peer appearance-none rounded-lg"
-      />
+      <input type="checkbox" className="left-1/2 hidden peer rounded-lg" />
       <FaSun className="text-gray-300 mr-2 h-6 w-6" />
       <span className="w-16 h-10 flex relative items-center flex-shrink-0 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:after:bg-black/75 peer-checked:bg-gray-700 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
       <FaMoon className="text-black-800 ml-2 h-6 w-6" />
